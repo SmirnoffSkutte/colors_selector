39ecd9628cc98edd84b8679c60bf71e3ab0accca
SmirnoffSkutte <smirnovivan963@gmail.com>
2022-03-25 19:23:52 +0300

    123
    123

--- src/App.js
+++ src/App.js
@@ -14,13 +14,37 @@ function App() {
   
   console.log(uColors)
   const mapColors=[...uColors]
- 
+
+  let rules = {};
+  for (let i = 0; i < mapColors.length; ++i) {
+    let color = mapColors[i];
+    rules[color] = [];
+    for (let j = 0; j < document.styleSheets.length; ++j) {
+      let styleSheet = document.styleSheets[j];
+      for (let k = 0; k < styleSheet.cssRules.length; ++k) {
+        let rule = styleSheet.cssRules[k];
+        if (rule.style && color == rule.style.color) {
+          rules[color].push(rule.selectorText);
+        }
+      }
+    }
+  }
+  console.log(rules)
+
+  function showrules(item){
+    alert(item)
+  }
 
   return (
     <div className="App">
-         {mapColors.map(plate=><div style={{backgroundColor : `${plate}`}} className='plate'></div>)}
-         Всего цветов:{mapColors.length}
-         
+      {Object.keys(rules).map((key)=>
+      {const item = rules[key];
+        return(
+            <div style={{backgroundColor : `${key}`}}  className='plate' value={item} onClick={()=>showrules(item)}></div>
+        )
+      }
+      )}
+          Всего цветов:{mapColors.length} 
     </div>
   );
 }
